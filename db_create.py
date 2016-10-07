7e1bebc02cbad4d1dfe9ebd3f5665c9224c04718
Tom Wright <tom@maladmin.com>
2016-10-07 15:11:07 -0400

    Fixed db_versioning scripts
    Scripts didn't work as db was designed under sqlalchemy not flask-sqalchemy.
    Moved all dependencies to flask-sqlalchemy

--- db_create.py
+++ db_create.py
@@ -2,14 +2,13 @@
 from migrate.versioning import api
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
-from app import database
+from app import db
 import os.path
 
 """ Generic script to create a database that supports migration.
     Database locations are defined in config.py """
+db.create_all()
 
-
-database.init_db()
 if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
     api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
     api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
