1d4d1bfe2ed701759fc51f87c843bb6b4f650cd3
Tom Wright <tom@maladmin.com>
2016-10-13 15:00:44 -0400

    Adding extra project info

--- add_study_info.py
+++ add_study_info.py
@@ -1,20 +1,33 @@
-"""Add extra study information into the database.
-"""
+"""Add extra study information into the database."""
 
 from app import db
-from app.models import Study, Person
+from app.models import Study, Person, get_if_exists
 
-andt = Study.query.filter(Study.nickname == 'ANDT').first()
+new_info = {study}
+
+
+andt = get_if_exists(Study(nickname='ANDT'))
+
 andt.description = 'Comparison of Underweight vs. Weight-Recovered Subjects ' \
                    'and Healthy Controls'
 andt.fullname = 'A Pilot Study of Diffusion Tensor Imaging in Anorexia ' \
                 'Nervosa: Comparison of Underweight vs. Weight-Recovered ' \
                 'Subjects and Healthy Controls'
 
-contact = Person()
-contact.name = "Amy Miles"
-contact.email = "amy.miles@camh.ca"
+andt_contact = get_if_exists(Person(email="amy.miles@camh.ca"))
+andt_contact.name = "Amy Miles"
 
-andt.primary_contact = contact
+andt.primary_contact = andt.contact
+##############
+asdd = get_if_exists(Study(nickname='ASDD'))
 
-db.session.commit()
+asdd.fullname = 'Autism Spectrum Disorder Study'
+
+asdd_contact = get_if_exists(Person(email="stephanie.ameis@camh.ca"))
+asdd_contact.name = "Stephanie Ameis"
+
+asdd.primary_contact = asdd_contact
+###############
+cogbdo = get_if_exists(Study(nickname='COGBDO'))
+
+cogbdo.description =
