b0a9985b90407ccfe0c7197dd97d8eefe6bd2d1e
Tom Wright <tom@maladmin.com>
2016-10-20 12:34:28 -0400

    Added logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,34 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 #from app.database import db_session
 db = SQLAlchemy(app)
+
+if not app.debug:
+    import logging
+    from logging.handlers import SMTPHandler, RotatingFileHandler
+    credentials = None
+    if MAIL_USERNAME or MAIL_PASSWORD:
+        credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT),
+                                'no-reply@kimellab.ca',
+                                ADMINS,
+                                'Dashboard failure',
+                                credentials)
+    file_handler = RotatingFileHandler('logs/dashboard.log',
+                                       'a',
+                                       1 * 1024 * 1024,
+                                       10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
+    app.logger.addHandler(file_handler)
+    app.logger.info('Dashboard startup')
 
 from app import views, models
--- config.py
+++ config.py
@@ -8,3 +8,12 @@ SECRET_KEY = 'bet-you-cant-guess'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,'dashboard.sqlite')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True #This should be turned off after development
+
+# mail server settings
+MAIL_SERVER = "smtp.camh.net"
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+# administrator list
+ADMINS = ['tom@maladmin.com']
