54d7badaadeb4c93d4a00d37c430a6829265665f
Tom Wright <tom@maladmin.com>
2016-10-21 11:40:53 -0400

    Turned off debugger pin

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 #!python
-
+import os
 from app import app
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
 """Start a local webserver for the flask app"""
-
+# disable the debuggering pincode
+os.environ["WERKZEUG_DEBUG_PIN"] = "off"
 app.run(debug=True, host='0.0.0.0')
