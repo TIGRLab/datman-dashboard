c1bbd3ce1fa9c33df7ec60e3ba681002143e957b
Tom Wright <tom@maladmin.com>
2016-10-25 10:57:30 -0400

    Another commit for alterante http server

--- run.py
+++ run.py
@@ -8,3 +8,6 @@ logging.basicConfig(level=logging.DEBUG)
 # disable the debuggering pincode
 os.environ["WERKZEUG_DEBUG_PIN"] = "off"
 app.run(debug=True, host='0.0.0.0')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
