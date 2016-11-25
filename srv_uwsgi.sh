79eccb3974034df5fa6fde3ea6e419e5e715aee9
Tom Wright <tom@maladmin.com>
2016-11-25 11:04:35 -0500

    Modified to run on new server

--- srv_uwsgi.sh
+++ srv_uwsgi.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-uwsgi --socket 0.0.0.0:5001 --protocol=http -w wsgi:app --enable-threads --static-map /archive=/mnt/tigrlab/archive
+uwsgi --plugins-dir /usr/lib/uwsgi/ --need-plugin python --socket 0.0.0.0:5001 --protocol=http --wsgi-file wsgi.py --callable app --enable-threads --static-map /archive=/mnt/tigrlab/archive
