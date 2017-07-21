a1e7f58a9cdf868b5027bc11519fde07cbb46b8e
Dawson Overton <djoverton@gmail.com>
2017-07-21 17:06:59 -0400

    Documentation for plot.js

--- dashboard/static/js/plot.js
+++ dashboard/static/js/plot.js
@@ -3,48 +3,62 @@ function get_src(d, element){
   window.location.href = '/session/' + session_id
 }
 
+/**
+ * Simple function to generate an array of numbers from 0 to length - 1
+ */
 function gen_range(length){
-  // simple function to generate an array of numbers from 0 to length - 1
   return Array.apply(null, Array(length)).map(function (_, i) {return i;});
 
 }
 
+/**
+ * initPlot takes an HTML element and list of objects and draws a metric plot using c3 and d3.
+ * 
+ */
 function initPlot(element, data){
 
-//var dataFromSelection = document.getElementById('data');
-//var dataList = JSON.parse(dataFromSelection.innerHTML);
+//Check if database query was successful.
 var dataList = data;
 if (typeof(dataList) == "undefined") {
   return;
 }
+
+//Sets to keep track of unique sites and scantypes.
 var siteSet = new Set();
 var site_scantype_set = new Set();
 
+//Lists for metrics, subjects, session names.
 var valueList = [];
 var subjectList = [];
 var sessionNameList = [];
+
+//List of lists (to hold metrics for each site separately).
 var overallValueList = [];
 session_lengths = []
 overallSubjectList = {};
 overallSessionNameList = {};
 
+//Generate set of unique sites and scantypes.
 dataList.forEach(function(entry){
   siteSet.add(entry.site_name);
   site_scantype_set.add(entry.site_name + ':' + entry.scan_description);
 });
 
+//For each scantype/site combination,
 site_scantype_set.forEach(function(site_scantype){
   valueList = [];
   subjectList = [];
   sessionNameList = [];
   dataList.forEach(function(entry){
     entry_site_scantype = entry.site_name + ':' + entry.scan_description;
+    //add metric value (and subject/session names) to list if it matches combination.
     if (entry_site_scantype == site_scantype) {
       valueList.push(entry.value);
       subjectList.push(entry.session_id);
       sessionNameList.push(entry.session_name);
     }
   } );
+  //"Unshift" is prepending to a list.
   valueList.unshift(site_scantype);
   overallValueList.push(valueList);
   session_lengths.push(subjectList.length);
@@ -54,6 +68,7 @@ site_scantype_set.forEach(function(site_scantype){
 
 var max_session_length = Math.max.apply(null, session_lengths);
 
+//Draw chart.
 var chart = c3.generate({
     bindto: element,
     data: {
@@ -72,6 +87,7 @@ var chart = c3.generate({
         }
       }
     },
+    //Settings to customize tooptip (shown when hovering datapoint).
     tooltip: {
     grouped: false,
     contents: function(d, defaultTitleFormat, defaultValueFormat, color) {
@@ -83,6 +99,7 @@ var chart = c3.generate({
       return text + "</table>";
     }
 },
+//Allow rescaling of x axis.
 zoom: {
     enabled: true
 }
