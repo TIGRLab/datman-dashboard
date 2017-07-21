14c9d0be16b447ff7921663d8d606f63f657317d
Tom Wright <tom@maladmin.com>
2017-07-21 11:29:49 -0400

    Bugfix: typo

--- dashboard/views.py
+++ dashboard/views.py
@@ -245,7 +245,7 @@ def create_issue(session_id, issue_title="", issue_body=""):
 @app.route('/session/<int:session_id>', methods=['GET', 'POST'])
 @app.route('/session/<int:session_id>/delete/<delete>', methods=['GET', 'POST'])
 @app.route('/session/<int:session_id>/flag_finding/<flag_finding>', methods=['GET', 'POST'])
-@login_requiredelse:
+@login_required
 def session(session_id=None, delete=False, flag_finding=False):
     """
     Default view for a single session_id
