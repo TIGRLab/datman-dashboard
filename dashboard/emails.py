8d49c2a596fe40acad3e3bda7ed469eb55730d79
Dawn E. Smith <Dawn.Smith@camh.ca>
2018-11-09 16:40:39 -0500

    Added email functions for approving/rejecting account requests

--- dashboard/emails.py
+++ dashboard/emails.py
@@ -1,8 +1,12 @@
+import logging
 from threading import Thread
 
+from flask import url_for
 from flask_mail import Message
 
-from dashboard import app, mail, SENDER, ADMINS
+from dashboard import app, mail, SENDER, ADMINS, DASH_SUPPORT
+
+logger = logging.getLogger(__name__)
 
 def async(f):
     def wrapper(*args, **kwargs):
@@ -18,6 +22,8 @@ def send_async_email(app, email):
 def send_email(subject, body, html_body=None, recipient=None):
     if not recipient:
         recipient = ADMINS
+    if not isinstance(recipient, list):
+        recipient = [recipient]
     email = Message(subject, sender=SENDER, recipients=recipient)
     email.body = body
     if html_body:
@@ -33,16 +39,38 @@ def incidental_finding_email(user, timepoint, comment):
 def account_request_email(first_name, last_name):
     subject = "New account request from {} {}".format(first_name,
             last_name)
-    dest_url = url_for('manage_users', _external=True)
-    html_body = "{} {} has requested dashboard access. <a href='{}'>" \
-            "Click here</a> to review and approve/reject the request".format(
-            first_name, last_name, dest_url)
     body = "{} {} has requested a dashboard account. Please log in to "\
             "approve or reject this request".format(first_name, last_name)
+    try:
+        dest_url = url_for('manage_users', _external=True)
+    except:
+        html_body = body
+    else:
+        html_body = "{} {} has requested dashboard access. <a href='{}'>" \
+                "Click here</a> to review and approve/reject the request".format(
+                first_name, last_name, dest_url)
     send_email(subject, body, html_body=html_body)
 
 def account_activation_email(user):
-    return
+    if not user.email:
+        logger.error("Can't send account activation email to user {}. No "
+                "email address available.".format(user.id))
+        return
+    subject = "QC Dashboard account activated"
+    body = "You can now log in to the QC dashboard using account {}. You "\
+            "can currently access {} studies. Access to additional studies "\
+            "can be requested by filling in the study request form found on "\
+            "your profile page after logging in.".format(user.username,
+            len(user.studies))
+    send_email(subject, body, recipient=user.email)
 
 def account_rejection_email(user):
-    return
+    if not user.email:
+        logger.error("Can't send account request rejection email to user {}."
+                "No email address available".format(user.id))
+        return
+    subject = "QC Dashboard account request rejected"
+    body = "An admin has reviewed your request for access to the QC dashboard "\
+            "and unfortunately it has been rejected. For any questions you "\
+            "may have please contact us at {}".format(DASH_SUPPORT)
+    send_email(subject, body, recipient=user.email)
