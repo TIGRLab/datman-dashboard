b27feb302fa05cdaf93b656f14af7283c06fcc8f
Dawn E. Smith <Dawn.Smith@camh.ca>
2019-02-05 17:21:41 -0500

    Removed line that wasnt actually doing anything

--- run.py
+++ run.py
@@ -11,8 +11,6 @@ Useful for debug / development.
 Some functionality (particularly file system access is disabled)
 
 """
-# disable the debuggering pincode
-os.environ["WERKZEUG_DEBUG_PIN"] = "off"
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True, host='0.0.0.0')
