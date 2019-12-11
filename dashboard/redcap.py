f5ffe095da9ecf9ff9ca42002483c7510fa195eb
Dawn E. Smith <Dawn.Smith@camh.ca>
2019-12-11 12:52:25 -0500

    Made redcap module py3 and pep8 compatible

--- dashboard/redcap.py
+++ dashboard/redcap.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import
+#!/usr/bin/env python
 
 import re
 import logging
@@ -22,12 +22,13 @@ def create_from_request(request):
         project = request.form['project_id']
         url = request.form['redcap_url']
         instrument = request.form['instrument']
-        version = re.search('redcap_v(.*)\/index',
-                request.form['project_url']).group(1)
+        version = re.search('redcap_v(.*)/index',
+                            request.form['project_url']).group(1)
         completed = int(request.form[instrument + '_complete'])
     except KeyError:
         raise RedcapException('Redcap data entry trigger request missing a '
-                'required key. Found keys: {}'.format(request.form.keys()))
+                              'required key. Found keys: {}'.format(
+                                        list(request.form.keys())))
 
     if completed != 2:
         logger.info("Record {} not completed. Ignoring".format(record))
@@ -41,7 +42,7 @@ def create_from_request(request):
                 record, url))
     elif len(server_record) > 1:
         raise RedcapException('Found {} records matching {} on redcap server '
-                '{}'.format(len(server_record), record, url))
+                              '{}'.format(len(server_record), record, url))
 
     server_record = server_record[0]
 
@@ -52,20 +53,23 @@ def create_from_request(request):
         session_name = server_record['par_id']
     except KeyError:
         raise RedcapException('Redcap record {} from server {} missing a '
-                'required field. Found keys: {}'.format(record,
-                server_record.keys()))
+                              'required field. Found keys: {}'.format(
+                                    record,
+                                    list(server_record.keys())))
 
     session = set_session(session_name)
     try:
         new_record = session.add_redcap(record, project, url, instrument, date,
-                redcap_user, comment, version)
+                                        redcap_user, comment, version)
     except Exception as e:
         raise RedcapException("Failed adding record {} from project {} on "
-                "server {}. Reason: {}".format(record, project, url, e))
+                              "server {}. Reason: {}".format(record, project,
+                                                             url, e))
 
     monitor_scan_import(session)
 
     return new_record
+
 
 def set_session(name):
     name = name.upper()
@@ -84,12 +88,14 @@ def set_session(name):
 
     return session
 
+
 def find_study(ident):
     study = get_study(tag=ident.study, site=ident.site)
     if not study:
         raise RedcapException("Invalid study/site combination: {} {}"
-            "".format(ident.study, ident.site))
+                              "".format(ident.study, ident.site))
     return study
+
 
 def get_timepoint(ident):
     timepoint = Timepoint.query.get(ident.get_full_subjectid_with_timepoint())
