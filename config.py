c87db094423842fa967bb025ab4ec6dbc900f6e4
Dawn E. Smith <Dawn.Smith@camh.ca>
2020-01-10 14:46:32 -0500

    Updated logging config (moved to dictConfig) + fixed scheduler bugs

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-"""
-Config file for dashboard webapp
-In production environment variables are defined in:
-    /etc/uwsgi/apps-available/dashboard.ini
+"""Collects all configuration information for the dashboard
 """
 import os
+from pathlib import Path
+import logging
+from logging.handlers import DEFAULT_TCP_LOGGING_PORT
 
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from flask_apscheduler.auth import HTTPBasicAuth
@@ -18,6 +18,10 @@ def read_boolean(var_name, default=False):
     if result == 'true' or result == 'on':
         return True
     return False
+
+BASE_DIR = Path(__file__).parent
+ENV = os.environ.get('FLASK_ENV')
+DEBUG = os.environ.get('FLASK_DEBUG') or (ENV and 1)
 
 ## Flask settings ##############################################################
 WTF_CSRF_ENABLED = True
@@ -35,8 +39,7 @@ SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{pwd}@{srvr}/{db}'.format(
 # See https://docs.python.org/2/library/datetime.html#datetime.datetime.now
 TZ_OFFSET = os.environ.get('TIMEZONE') or -240
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'migrations')
+SQLALCHEMY_MIGRATE_REPO = BASE_DIR / 'migrations'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 ################################################################################
@@ -71,15 +74,6 @@ MAIL_PASSWORD = os.environ.get("DASHBOARD_MAIL_PASS") or None
 DASH_SUPPORT = os.environ.get("DASHBOARD_SUPPORT_EMAIL") or MAIL_USERNAME
 SENDER = DASH_SUPPORT or MAIL_USERNAME or "no-reply@kimellab.ca"
 MAIL_USE_SSL = read_boolean("DASHBOARD_MAIL_SSL", default=True)
-
-################################################################################
-## Logging settings ############################################################
-LOGSERVER = os.environ.get("DASHBOARD_LOG_SERVER")
-
-LOG_MAIL_SERVER = os.environ.get("DASH_LOG_EMAIL") or 'smtp.camh.net'
-LOG_MAIL_PORT = os.environ.get("DASH_LOG_PORT") or 25
-LOG_MAIL_USER = None
-LOG_MAIL_PASS = None
 
 ################################################################################
 ## Scheduler settings ##########################################################
@@ -119,7 +113,7 @@ SCHEDULER_PASS = os.environ.get('DASHBOARD_SCHEDULER_PASS')
 SCHEDULER_SERVER_URL = os.environ.get("DASHBOARD_URL")
 
 ################################################################################
-## Other settings ##############################################################
+## Misc. settings ##############################################################
 
 # administrator email list
 try:
@@ -140,5 +134,90 @@ DISPLAY_METRICS = {'phantom': {'t1': ['c1', 'c2', 'c3', 'c4'],
                             'dti': ['tsnr_bX', 'meanRELrms', '#ndirs',
                                     'Spikecount'],
                             'fmri': ['mean_fd', 'mean_sfnr', 'ScanLength']}}
+
+################################################################################
+## Logging settings ############################################################
+
+LOG_LEVEL = os.environ.get('DASH_LOG_LEVEL') or 'DEBUG'
+
+LOGGING_CONFIG = {
+    'version': 1,
+    'formatters': {
+        'basic': {
+            'class': 'logging.Formatter',
+            'format': '%(asctime)s [%(name)s] %(levelname)s - %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': LOG_LEVEL,
+            'formatter': 'basic'
+        }
+    },
+    'loggers': {
+        'dashboard': {
+            'handlers': ['console']
+        }
+    }
+}
+
+LOG_SERVER = os.environ.get("DASHBOARD_LOG_SERVER")
+if LOG_SERVER:
+    LOG_PORT = (os.environ.get("DASHBOARD_LOG_SERVER_PORT")
+                or DEFAULT_TCP_LOGGING_PORT)
+    LOGGING_CONFIG['handlers']['server'] = {
+        'class': 'logging.handlers.SocketHandler',
+        'host': LOG_SERVER,
+        'port': LOG_PORT,
+        'level': LOG_LEVEL,
+        'formatter': 'basic',
+    }
+    LOGGING_CONFIG['loggers']['dashboard']['handlers'].append('server')
+
+if not DEBUG:
+    LOG_MAIL_SERVER = os.environ.get("DASH_LOG_MAIL_SERVER") or 'smtp.camh.net'
+    LOG_MAIL_PORT = os.environ.get("DASH_LOG_MAIL_PORT") or 25
+    LOG_MAIL_USER = os.environ.get("DASH_LOG_MAIL_USER")
+    LOG_MAIL_PASS = os.environ.get("DASH_LOG_MAIL_PASS")
+    if LOG_MAIL_USER or LOG_MAIL_PASS:
+        credentials = (LOG_MAIL_USER, LOG_MAIL_PASS)
+    else:
+        credentials = None
+
+    LOGGING_CONFIG['handlers']['email'] = {
+        'class': 'logging.handlers.SMTPHandler',
+        'mailhost': (LOG_MAIL_SERVER, LOG_MAIL_PORT),
+        'fromaddr': SENDER,
+        'toaddrs': ADMINS,
+        'subject': 'Dashboard failure',
+        'credentials': credentials,
+        'formatter': 'basic',
+        'level': 'ERROR'
+    }
+    LOGGING_CONFIG['loggers']['dashboard']['handlers'].append('email')
+
+if DEBUG and ENV == 'development':
+    user_dir = os.environ.get('DASH_LOG_DIR')
+    LOG_DIR = Path(user_dir) if user_dir else BASE_DIR / 'logs'
+    LOG_DIR.mkdir(mode=0o755, parents=False, exist_ok=True)
+
+    DEST_LOG = LOG_DIR / 'dashboard.log'
+    try:
+        DEST_LOG.touch()
+    except (PermissionError, FileNotFoundError):
+        pass
+    else:
+        LOGGING_CONFIG['handlers']['log_file'] = {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': DEST_LOG,
+            'mode': 'a',
+            'maxBytes': 1 * 1024 * 1024,
+            'backupCount': 10,
+            'formatter': 'basic',
+            'level': LOG_LEVEL,
+            }
+        LOGGING_CONFIG['loggers']['dashboard']['handlers'].append('log_file')
+
 
 ################################################################################
