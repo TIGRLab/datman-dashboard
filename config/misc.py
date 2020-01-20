d1fe21eda3dc012a33483efcef706d962491e6a4
Dawn E. Smith <Dawn.Smith@camh.ca>
2020-01-20 16:32:23 -0500

    Moved flask config into misc.py

--- config/misc.py
+++ config/misc.py
@@ -4,6 +4,17 @@ import os
 
 from .utils import read_boolean
 
+# This should never be False
+WTF_CSRF_ENABLED = True
+
+SECRET_KEY = os.environ.get('FLASK_SECRET_KEY')
+
+if not SECRET_KEY:
+    raise RuntimeError(
+        "Must have 'FLASK_SECRET_KEY' set to start the app. This setting is "
+        "used to encrypt session information and cookies and should be a "
+        "random / hard to guess string.")
+
 # Name of the repository on github that hosts the issues
 GITHUB_REPO = os.environ.get('GITHUB_ISSUES_REPO')
 
