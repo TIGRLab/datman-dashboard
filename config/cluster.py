a65bcd8e248e98f0ddd70d440a74f9d2a989ffe2
Dawn Smith <dawn.smith@camh.ca>
2020-07-27 18:59:48 -0400

    Added overrideable path for queue job scripts

--- config/cluster.py
+++ config/cluster.py
@@ -7,3 +7,13 @@ SUBMIT_COMMAND = os.environ.get("DASHBOARD_QSUBMIT_CMD") or "sbatch"
 
 # Options to always set during submission (e.g. QOS)
 SUBMIT_OPTIONS = os.environ.get("DASHBOARD_QSUBMIT_OPTIONS") or ""
+
+# Job script location. If changed from dashboard/queue_jobs, the folder
+# must contain scripts matching the names of those in the original folder.
+SUBMIT_SCRIPTS = (
+    os.environ.get("DASHBOARD_QSUBMIT_SCRIPTS") or
+    os.path.join(
+        os.path.dirname(os.path.realpath(__name__)),
+        '..dashboard/queue_jobs'
+        )
+    )
