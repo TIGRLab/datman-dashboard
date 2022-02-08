1ea263444ccc9c35fc47d530aee7160632470083
Dawn Smith <dawn.smith@camh.ca>
2022-02-08 14:33:58 -0500

    [ENH] Add tests describing correct behavior for redcap utils set_session

--- tests/test_redcap_bp_utils.py
+++ tests/test_redcap_bp_utils.py
@@ -5,6 +5,39 @@ import dashboard.blueprints.redcap.utils as rc_utils
 import datman.scanid
 
 
+class TestSetSession:
+
+    def test_valid_id_returns_a_dashboard_session_record(self, records):
+        name = "STU01_CMH_0001_01_01"
+        session = rc_utils.set_session(name)
+        assert str(session) == name
+
+    def test_valid_id_with_space_padding_is_accepted(self, dash_db):
+        name = " STU01_CMH_0001_01_01 "
+        session = rc_utils.set_session(name)
+        assert session is not None
+        assert str(session) == name.strip()
+
+    def test_id_with_undefined_site_raises_exception(self, dash_db):
+        with pytest.raises(dashboard.exceptions.RedcapException):
+            rc_utils.set_session("STU01_ABC_0001_01_01")
+
+    def test_id_with_undefined_study_raises_exception(self, dash_db):
+        with pytest.raises(dashboard.exceptions.RedcapException):
+            rc_utils.set_session("AAA00_CMH_0001_01_01")
+
+    def test_id_with_unexpected_format_raises_exception(self, dash_db):
+        with pytest.raises(dashboard.exceptions.RedcapException):
+            rc_utils.set_session("STUDYCMH-1000")
+
+    @pytest.fixture
+    def records(self, dash_db):
+        study = dashboard.models.Study("STUDY1")
+        dash_db.session.add(study)
+        study.update_site("CMH", code="STU01", create=True)
+        return [study]
+
+
 class TestGetTimepoint:
 
     def test_can_find_timepoint_by_datman_ident(self, study, ident):
