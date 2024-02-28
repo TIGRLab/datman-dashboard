8f735717560a00970f4cae1c691b31ceda808b38
Dawn Smith <dawn.smith@camh.ca>
2024-02-28 17:06:18 -0500

    [ENH] Add config handler for user menu configuration

---
+++ config/menu.py
@@ -0,0 +1,49 @@
+"""Configuration to modify menu contents.
+"""
+import logging
+import os
+
+from pydantic import BaseModel, ValidationError
+import yaml
+
+logger = logging.getLogger(__name__)
+
+
+class MenuItem(BaseModel):
+    menu: str
+    btn_text: str
+    url: str
+    hover_text: str = None
+    name: str = None
+
+
+def get_menu_config():
+    menu_file = os.environ.get("DASH_MENU_CONFIG")
+
+    if not menu_file:
+        return {}
+
+    try:
+        with open(menu_file) as fh:
+            contents = yaml.load(fh, Loader=yaml.SafeLoader)
+    except (FileNotFoundError, PermissionError, yaml.parser.ParserError) as e:
+        logger.error(f"Could not read 'DASH_MENU_CONFIG' file {menu_file}."
+                     "Custom menu items can not be added. Reason - {e}")
+        return {}
+
+    menu_config = {}
+    for btn in contents:
+        try:
+            item = MenuItem(name=btn, **contents[btn])
+        except ValidationError as e:
+            logger.error(
+                f"Skipping entry '{btn}' in DASH_MENU_CONFIG' file {menu_file}"
+                f" Reason - {e}"
+            )
+            continue
+        menu_config.setdefault(item.menu, []).append(item)
+
+    return menu_config
+
+
+MENU_CONFIG = get_menu_config()
